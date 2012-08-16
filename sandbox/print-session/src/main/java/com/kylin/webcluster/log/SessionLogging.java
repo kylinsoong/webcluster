12125f96a6f66b8f73428bba15f4e403d5a98562
Kylin <songzhiqi_1214@yahoo.com.cn>
2012-08-16 17:30:19 +0800

    update webcuster

--- sandbox/print-session/src/main/java/com/kylin/webcluster/log/SessionLogging.java
+++ sandbox/print-session/src/main/java/com/kylin/webcluster/log/SessionLogging.java
@@ -8,23 +8,27 @@ public class SessionLogging {
 	
 	public void logRequest(RequestBean bean) {
 		
-		logger.info("Logging request start");
-		logger.debug("Path: " + bean.getBasePath());
-		logger.debug("Protocol: " + bean.getProtocol());
-		logger.debug("Servlet Path: " + bean.getServletPath());
-		logger.debug("Remote Address: " + bean.getRemoteAddr());
-		logger.debug("Remote Host: " + bean.getRemoteHost());
-		logger.debug("Remote Port" + bean.getRemotePort());
-		logger.debug("Remote User: " + bean.getRemoteUser());
-		logger.debug("Local Address: " + bean.getLocalAddr());
-		logger.debug("Local Name: " + bean.getLocalName());
-		logger.debug("Local Host: " + bean.getLocalPort());
-		logger.debug("Request URL: " + bean.getRequestUrl());
-		logger.info("Request Session ID: " + bean.getRequestSessionId());
 		logger.info("Session ID: " + bean.getSessionId());
-		logger.debug("Session Creation Time: " + bean.getCreationDate());
-		logger.debug("Session Last Access Time: " + bean.getLastAccessTime());
-		logger.info("-----------------------------");
+		
+		if(logger.isDebugEnabled()) {
+			logger.debug("Logging request start");
+			logger.debug("Path: " + bean.getBasePath());
+			logger.debug("Protocol: " + bean.getProtocol());
+			logger.debug("Servlet Path: " + bean.getServletPath());
+			logger.debug("Remote Address: " + bean.getRemoteAddr());
+			logger.debug("Remote Host: " + bean.getRemoteHost());
+			logger.debug("Remote Port" + bean.getRemotePort());
+			logger.debug("Remote User: " + bean.getRemoteUser());
+			logger.debug("Local Address: " + bean.getLocalAddr());
+			logger.debug("Local Name: " + bean.getLocalName());
+			logger.debug("Local Host: " + bean.getLocalPort());
+			logger.debug("Request URL: " + bean.getRequestUrl());
+			logger.debug("Request Session ID: " + bean.getRequestSessionId());
+			logger.debug("Session Creation Time: " + bean.getCreationDate());
+			logger.debug("Session Last Access Time: " + bean.getLastAccessTime());
+			logger.debug("-----------------------------");
+		}
+		
 	}
 
 }
