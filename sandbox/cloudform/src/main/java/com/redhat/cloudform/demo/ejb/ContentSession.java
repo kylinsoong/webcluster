ca16055765bfe1c4ad9a32cbeba41c93c7cd115f
Kylin <songzhiqi_1214@yahoo.com.cn>
2012-08-28 19:59:10 +0800

    change cloudform demo code for support cluster

--- sandbox/cloudform/src/main/java/com/redhat/cloudform/demo/ejb/ContentSession.java
+++ sandbox/cloudform/src/main/java/com/redhat/cloudform/demo/ejb/ContentSession.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
-import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
